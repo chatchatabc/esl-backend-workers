dc622c107937622d6419a4cfb01ac4019ba91759
BoJoNVi <bonjomontes@gmail.com>
2023-07-17 17:01:46 +0800

    improv: added a role column for the users table

--- schema.sql
+++ schema.sql
@@ -1,3 +1,3 @@
 DROP TABLE IF EXISTS users;
-CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT, phone TEXT, createdAt TEXT, updatedAt TEXT);
-INSERT INTO users (id, username, password, phone, createdAt, updatedAt) VALUES (1, 'admin', 'admin', '09123456788', '2019-01-01 00:00:00', '2019-01-01 00:00:00');
+CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT, phone TEXT, createdAt TEXT, updatedAt TEXT, role INTEGER);
+INSERT INTO users (id, username, password, phone, createdAt, updatedAt, role) VALUES (1, 'admin', 'admin', '09123456788', '2019-01-01 00:00:00', '2019-01-01 00:00:00', 1);

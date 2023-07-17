342f92301dd1ae0dbd4f1371ca641377081830a6
BoJoNVi <bonjomontes@gmail.com>
2023-07-17 14:31:29 +0800

    chore: initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { Hono } from "hono";
+import rest from "./application/rest";
+import { CommonEnv } from "./domain/models/CommonModel";
+
+const app = new Hono<CommonEnv>();
+
+app.route("/api", rest);
+
+app.all("*", (c) => c.text("Not Found", 404));
+
+export default app;

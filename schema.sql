992d9b0e566c258e5a6baf53b7e2fd479f0632ff
BoJoNVi <bonjomontes@gmail.com>
2023-07-18 09:32:29 +0800

    refactor: changed the password of admin during population of users table

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT, phone TEXT, createdAt TEXT, updatedAt TEXT, role INTEGER);
-INSERT INTO users (id, username, password, phone, createdAt, updatedAt, role) VALUES (1, 'admin', '71fcd0d56313d0bf75412606f2c989153f6a474aa9200937f2a556875eee7bbe', '09123456788', '2019-01-01 00:00:00', '2019-01-01 00:00:00', 1);
+INSERT INTO users (id, username, password, phone, createdAt, updatedAt, role) VALUES (1, 'admin', 'da023f7090dd831097f8a534475b1c4fba2a9a6419968e52be7459e2533ac819', '09123456788', '2019-01-01 00:00:00', '2019-01-01 00:00:00', 1);
 
 DROP TABLE IF EXISTS roles;
 CREATE TABLE IF NOT EXISTS roles (id INTEGER PRIMARY KEY, name TEXT, createdAt TEXT, updatedAt TEXT);

f601c7d8d41db275d0bc9648b6dfa078880b733d
BoJoNVi <bonjomontes@gmail.com>
2023-07-18 09:32:57 +0800

    feat: added cors to the rest controller

--- src/application/rest/index.ts
+++ src/application/rest/index.ts
@@ -1,9 +1,19 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { CommonEnv } from "../../domain/models/CommonModel";
 import auth from "./authController";
 import { honoFailedResponse } from "../../domain/services/honoService";
 
 const rest = new Hono<{ Bindings: CommonEnv }>();
+
+rest.use(
+  "*",
+  cors({
+    origin: ["http://localhost:3000"],
+    allowMethods: ["*"],
+    allowHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 rest.route("/auth", auth);
 

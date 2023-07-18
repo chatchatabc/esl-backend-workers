d871c7d8fa35a0950a9c0800a5660bec8044cffc
BoJoNVi <bonjomontes@gmail.com>
2023-07-18 09:58:53 +0800

    refactor: remove the code from userCreate to authRegister

--- src/domain/services/authService.ts
+++ src/domain/services/authService.ts
@@ -1,8 +1,13 @@
 import { SHA256 } from "crypto-js";
 import { Context } from "hono";
 import { CommonContext } from "../models/CommonModel";
-import { userDbGetByUsername } from "../repositories/userRepo";
+import {
+  userDbGetByPhone,
+  userDbGetByUsername,
+  userDbInsert,
+} from "../repositories/userRepo";
 import { honoFailedResponse } from "./honoService";
+import { roleDbGet } from "../repositories/roleRepo";
 
 const secret = "secret";
 
@@ -25,4 +30,38 @@ export async function authLogin(
 
   delete user.password;
   return c.json({ data: user }, 200);
+}
+
+export async function authRegister(
+  c: Context<CommonContext>,
+  body: Record<string, any>
+) {
+  // Check if user exists by username
+  let user = await userDbGetByUsername(c, body.username);
+  if (user) {
+    return honoFailedResponse(c, "Username already exists.", 400);
+  }
+
+  // Check if user exists by phone
+  user = await userDbGetByPhone(c, body.phone);
+  if (user) {
+    return honoFailedResponse(c, "Phone already exists.", 400);
+  }
+
+  // Check if role exist
+  const role = await roleDbGet(c, body.role);
+  if (!role) {
+    return honoFailedResponse(c, "Role does not exist.", 400);
+  }
+
+  // Insert user
+  const insert = await userDbInsert(c, body);
+  if (!insert) {
+    return honoFailedResponse(c, "Failed to create user.", 500);
+  }
+
+  // Return user and remove password
+  user = await userDbGetByUsername(c, body.username);
+  delete user?.password;
+  return c.json({ data: user });
 }
--- src/domain/services/userService.ts
+++ src/domain/services/userService.ts
@@ -3,44 +3,7 @@ import { CommonContext } from "../models/CommonModel";
 import {
   userDbGetByPhone,
   userDbGetByUsername,
-  userDbInsert,
 } from "../repositories/userRepo";
-import { roleDbGet } from "../repositories/roleRepo";
-import { honoFailedResponse } from "./honoService";
-
-export async function userCreate(
-  c: Context<CommonContext>,
-  body: Record<string, any>
-) {
-  // Check if user exists by username
-  let user = await userDbGetByUsername(c, body.username);
-  if (user) {
-    return honoFailedResponse(c, "Username already exists.", 400);
-  }
-
-  // Check if user exists by phone
-  user = await userDbGetByPhone(c, body.phone);
-  if (user) {
-    return honoFailedResponse(c, "Phone already exists.", 400);
-  }
-
-  // Check if role exist
-  const role = await roleDbGet(c, body.role);
-  if (!role) {
-    return honoFailedResponse(c, "Role does not exist.", 400);
-  }
-
-  // Insert user
-  const insert = await userDbInsert(c, body);
-  if (!insert) {
-    return honoFailedResponse(c, "Failed to create user.", 500);
-  }
-
-  // Return user and remove password
-  user = await userDbGetByUsername(c, body.username);
-  delete user?.password;
-  return c.json({ data: user });
-}
 
 export async function userGet(
   c: Context<CommonContext>,

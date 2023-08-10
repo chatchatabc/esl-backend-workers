b117fbc46fa3ca7fa9adc444f4cfded0a781723d
BoJoNVi <bonjomontes@gmail.com>
2023-08-10 09:33:07 +0800

    feat: utilities service

--- src/domain/services/utilService.ts
+++ src/domain/services/utilService.ts
@@ -1,6 +1,7 @@
 import { TRPCError } from "@trpc/server";
 import type { TRPC_ERROR_CODE_KEY } from "@trpc/server/dist/rpc";
 import { enc, HmacSHA256 } from "crypto-js";
+import { ScheduleCreate } from "../models/ScheduleModel";
 
 export function utilFailedResponse(message: string, status: number = 500) {
   let code: TRPC_ERROR_CODE_KEY = "INTERNAL_SERVER_ERROR";
@@ -41,4 +42,73 @@ export function utilDecodeBase64(input: string) {
 export function utilHashHmac256(input: string) {
   const secret = "I)0Don't!1Care@2";
   return HmacSHA256(input, secret).toString();
+}
+
+export function utilGetTimestampTimeOnly(timestamp: number) {
+  const hour = new Date(timestamp).getUTCHours();
+  const minute = new Date(timestamp).getUTCMinutes();
+  const timestampTimeOnly = hour * 60 * 60 * 1000 + minute * 60 * 1000;
+
+  return timestampTimeOnly;
+}
+
+export function utilGetTimestampDateOnly(timestamp: number) {
+  const day = new Date(timestamp).getUTCDay();
+
+  const date = new Date(0);
+  date.setUTCMonth(1);
+  date.setUTCDate(day + 1);
+
+  return date.getTime();
+}
+
+export function utilCheckScheduleOverlap(schedules: ScheduleCreate[]) {
+  let overlapped = false;
+  // Fix the day and convert the time to timestamp
+  const newSchedules = schedules.map((schedule, index) => {
+    const day = new Date(schedule.startTime).getUTCDay();
+    const startTime = utilGetTimestampTimeOnly(schedule.startTime);
+    const endTime = startTime + (schedule.endTime - schedule.startTime);
+    return {
+      ...schedule,
+      startTime,
+      endTime,
+      day,
+      id: index,
+    };
+  });
+
+  newSchedules.forEach((old) => {
+    const overlap = newSchedules.find((schedule) => {
+      return (
+        schedule.id !== old.id &&
+        schedule.day === old.day &&
+        ((schedule.startTime >= old.startTime &&
+          schedule.startTime < old.endTime) ||
+          (schedule.endTime > old.startTime && schedule.endTime <= old.endTime))
+      );
+    });
+
+    if (overlap) {
+      overlapped = true;
+    }
+  });
+
+  return overlapped;
+}
+
+export function utilValidateChineseMobileNumber(mobile: string) {
+  return /^1(?:3(?:4[^9\D]|[5-9]\d)|5[^3-6\D]\d|7[28]\d|8[23478]\d|9[578]\d)\d{7}$/.test(
+    mobile
+  );
+}
+
+export function utilGenerateRandomCode(length = 6) {
+  const characters = "0123456789";
+  let token = "";
+  for (let i = 0; i < length; i++) {
+    token += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return token;
 }

01aebd3eb6de95dafe08327ceff72bb6bf039f61
BoJoNVi <bonjomontes@gmail.com>
2023-08-10 09:50:03 +0800

    feat: add all router to the parent router

--- src/application/trpc/index.ts
+++ src/application/trpc/index.ts
@@ -1,9 +1,19 @@
-import authRouter from "./authRouter";
 import { trpcRouterCreate } from "../../domain/infra/trpc";
+import authRouter from "./authRouter";
+import bookingRouter from "./bookingRouter";
+import logsRouter from "./logsRouter";
+import userRouter from "./userRouter";
+import scheduleRouter from "./scheduleRouter";
+import teacherRouter from "./teacherRouter";
 
 // tRPC Main Router
 const trpcRouter = trpcRouterCreate({
   auth: authRouter,
+  booking: bookingRouter,
+  logs: logsRouter,
+  schedule: scheduleRouter,
+  user: userRouter,
+  teacher: teacherRouter,
 });
 
 export default trpcRouter;

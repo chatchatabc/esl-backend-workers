0b1f125b40c5f537b2fcb3501b8ec78cba34bb41
BoJoNVi <bonjomontes@gmail.com>
2023-08-10 09:46:14 +0800

    feat: updating and getting of user with userId

--- src/domain/repositories/userRepo.ts
+++ src/domain/repositories/userRepo.ts
@@ -31,3 +31,59 @@ export async function userDbInsert(body: UserRegister, env: Env) {
     return false;
   }
 }
+
+export async function userDbGet(params: { userId: number }, env: Env) {
+  const { userId } = params;
+
+  try {
+    const user = await env.DB.prepare("SELECT * FROM users WHERE id = ?")
+      .bind(userId)
+      .first<User>();
+
+    return user;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+}
+
+export async function userDbUpdate(params: User, env: Env) {
+  const {
+    id = null,
+    username = null,
+    password = null,
+    roleId = null,
+    firstName = null,
+    lastName = null,
+    phone = null,
+    email = null,
+    phoneVerifiedAt = null,
+    emailVerifiedAt = null,
+  } = params;
+
+  const date = new Date().getTime();
+  try {
+    const query = await env.DB.prepare(
+      "UPDATE users SET username = ?, password = ?, roleId = ?, updatedAt = ?, firstName = ?, lastName = ?, phone = ?, email = ?, phoneVerifiedAt = ?, emailVerifiedAt = ? WHERE id = ?"
+    )
+      .bind(
+        username,
+        password,
+        roleId,
+        date,
+        firstName,
+        lastName,
+        phone,
+        email,
+        phoneVerifiedAt,
+        emailVerifiedAt,
+        id
+      )
+      .run();
+
+    return true;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+}

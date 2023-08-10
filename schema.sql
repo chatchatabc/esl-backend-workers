68fb5f685825cfcbac8a4eaa1bfcff8e350d02b7
BoJoNVi <bonjomontes@gmail.com>
2023-08-10 15:03:04 +0800

    improv: further improve the schema table

--- schema.sql
+++ schema.sql
@@ -6,7 +6,8 @@
 * email (text?)
 * firstName (text?)
 * lastName (text?)
-* roleId (integer)
+* roleId (integer) [1: admin, 2: student, 3: teacher]
+* status (integer) [1: active, 2: inactive, -1: deleted]
 * credit (integer)
 * phoneVerifiedAt (timestamp?)
 * emailVerifiedAt (timestamp?)
@@ -14,20 +15,22 @@
 * updatedAt (timestamp)
 */
 DROP TABLE IF EXISTS users;
-CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT NOT NULL, password TEXT NOT NULL, phone TEXT, email TEXT, firstName TEXT, lastName TEXT, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL, roleId INTEGER NOT NULL, credit INTEGER NOT NULL, phoneVerifiedAt INTEGER, emailVerifiedAt INTEGER);
-INSERT INTO users (username, password, roleId, credit, createdAt, updatedAt, firstName, lastName, phone, phoneVerifiedAt) VALUES ('admin', 'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3', 1, 1000, 0, 0, 'Bon Jovi', 'Montes', '+639338520220', 1), ('student', 'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3', 2, 1000, 0, 0, 'Joy', 'Surname', '+891234567890', 1), ('teacher', 'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3', 3, 1000, 0, 0, 'Michelle', 'Surname', '+891234567890', 1);
+CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT NOT NULL, password TEXT NOT NULL, phone TEXT, email TEXT, firstName TEXT, lastName TEXT, status INTEGER NOT NULL, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL, roleId INTEGER NOT NULL, credit INTEGER NOT NULL, phoneVerifiedAt INTEGER, emailVerifiedAt INTEGER);
+INSERT INTO users (username, password, roleId, credit, createdAt, updatedAt, firstName, lastName, phone, phoneVerifiedAt, status) VALUES ('admin', 'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3', 1, 1000, 0, 0, 'Bon Jovi', 'Montes', '+639338520220', 1, 1), ('student', 'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3', 2, 1000, 0, 0, 'Joy', 'Surname', '+891234567890', 1, 1), ('teacher', 'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3', 3, 1000, 0, 0, 'Michelle', 'Surname', '+891234567890', 1, 1);
 
 /**
 * id (integer)
 * userId (integer)
+* alias (text)
 * price (integer)
-* interval (timestampTimeOnly)
+* minutes (integer)
+* status (integer) [1: active, 2: inactive, -1: deleted]
 * createdAt (timestamp)
 * updatedAt (timestamp)
 */
 DROP TABLE IF EXISTS teachers;
-CREATE TABLE IF NOT EXISTS teachers (userId INTEGER PRIMARY KEY, id INTEGER UNIQUE NOT NULL, name TEXT, alias TEXT, price INTEGER NOT NULL, interval INTEGER NOT NULL, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL);
-INSERT INTO teachers (userId, id, price, interval, createdAt, updatedAt) VALUES (3, 202307311, 50, 25, 0, 0);
+CREATE TABLE IF NOT EXISTS teachers (userId INTEGER PRIMARY KEY, id INTEGER UNIQUE NOT NULL, alias TEXT NOT NULL, price INTEGER NOT NULL, minutes INTEGER NOT NULL, status INTEGER NOT NULL, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL);
+INSERT INTO teachers (userId, id, price, alias, minutes, createdAt, updatedAt, status) VALUES (3, 202307311, 50, 25, 'Teacher Michelle', 0, 0, 1);
 
 
 /**
@@ -51,7 +54,6 @@ INSERT INTO roles (name, createdAt, updatedAt) VALUES ('admin', 0, 0), ('student
 */
 DROP TABLE IF EXISTS schedules;
 CREATE TABLE IF NOT EXISTS schedules (id INTEGER PRIMARY KEY, teacherId INTEGER NOT NULL, startTime INTEGER NOT NULL, endTime INTEGER NOT NULL, day INTEGER NOT NULL, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL);
-INSERT INTO schedules (teacherId, createdAt, updatedAt, startTime, endTime, day) VALUES (3, 0, 0, 0, 14400000, 1);
 
 /**
 * id (integer)
@@ -59,7 +61,7 @@ INSERT INTO schedules (teacherId, createdAt, updatedAt, startTime, endTime, day)
 * studentId (integer)
 * start (timestamp)
 * end (timestamp)
-* status (integer)
+* status (integer) [1: active, 2: finished, 3: cancelled, -1: deleted]
 * message (text)
 * createdAt (timestamp)
 * updatedAt (timestamp)
@@ -73,10 +75,36 @@ INSERT INTO bookings (teacherId, studentId, status, createdAt, updatedAt, start,
 * senderId (integer)
 * receiverId (integer)
 * amount (integer)
-* status (integer)
+* status (integer) [1: pending, 2: finished, 3: cancelled, -1: deleted]
 * createdAt (timestamp)
 * updatedAt (timestamp)
 */
 DROP TABLE IF EXISTS logsCredit;
 CREATE TABLE IF NOT EXISTS logsCredit (id INTEGER PRIMARY KEY, title TEXT, senderId INTEGER NOT NULL, receiverId INTEGER NOT NULL, amount INTEGER NOT NULL, status INTEGER NOT NULL, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL);
+
+/**
+* id (integer)
+* title (text)
+* senderId (integer)
+* receiverId (integer)
+* message (text)
+* cron (text)
+* sendAt (timestamp?)
+* status (integer) [1: pending, 2: finished, 3: failed, 4: cancelled, -1: deleted]
+* createdAt (timestamp)
+* updatedAt (timestamp)
+*/
+DROP TABLE IF EXISTS messageSchedules;
+CREATE TABLE IF NOT EXISTS messageSchedules (id INTEGER PRIMARY KEY, title TEXT NOT NULL, senderId INTEGER NOT NULL, receiverId INTEGER NOT NULL, message TEXT NOT NULL, cron TEXT NOT NULL, sendAt INTEGER, status INTEGER NOT NULL, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL);
+
+/**
+* id (integer)
+* title (text)
+* message (text)
+* status (integer)
+* createdAt (timestamp)
+* updatedAt (timestamp)
+*/
+DROP TABLE IF EXISTS messageTemplates;
+CREATE TABLE IF NOT EXISTS messageTemplates (id INTEGER PRIMARY KEY, title TEXT, senderId INTEGER NOT NULL, receiverId INTEGER NOT NULL, amount INTEGER NOT NULL, status INTEGER NOT NULL, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL);
 

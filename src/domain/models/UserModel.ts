e07ba3e059b408f51c5a73a84b9e69063f01f7e7
BoJoNVi <bonjomontes@gmail.com>
2023-08-14 14:06:01 +0800

    refactor: modified the User Models

--- src/domain/models/UserModel.ts
+++ src/domain/models/UserModel.ts
@@ -1,28 +1,28 @@
 export type User = {
   username: string;
   password?: string;
+  firstName?: string | null;
+  lastName?: string | null;
+  phone?: string | null;
+  email?: string | null;
 
   id: number;
   roleId: number;
   createdAt: number;
   updatedAt: number;
   credit: number;
-
-  role?: UserRole;
-} & UserPersonalInformation &
-  UserContactInformation;
-
-export type UserPersonalInformation = {
-  firstName?: string | null;
-  lastName?: string | null;
-};
-
-export type UserContactInformation = {
-  phone?: string | null;
-  email?: string | null;
   phoneVerifiedAt?: number | null;
   emailVerifiedAt?: number | null;
+
+  role?: UserRole;
 };
+
+export type UserPersonalInformation = Pick<User, "firstName" | "lastName">;
+
+export type UserContactInformation = Pick<
+  User,
+  "phone" | "email" | "phoneVerifiedAt" | "emailVerifiedAt"
+>;
 
 export type UserRole = {
   name: string;
@@ -32,16 +32,21 @@ export type UserRole = {
   id: number;
 };
 
-export type UserRegister = {
-  roleId: number;
-  credit: number;
-} & UserRegisterInput;
-
-export type UserRegisterInput = {
+export type UserRegisterInput = UserLogin & {
   confirmPassword: string;
-} & UserLogin;
+};
 
-export type UserLogin = {
-  username: string;
+export type UserLogin = Pick<User, "username"> & {
   password: string;
+};
+
+export type UserCreate = Omit<
+  User,
+  "id" | "createdAt" | "updatedAt" | "role"
+> & {
+  password: string;
+};
+
+export type UserCreateInput = UserCreate & {
+  confirmPassword: string;
 };

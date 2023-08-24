881145d81b96d51232961469ecd233c6c417c4c4
BoJoNVi <bonjomontes@gmail.com>
2023-08-24 08:30:35 +0800

    chore: format schema.sql

--- schema.sql
+++ schema.sql
@@ -1,113 +1,332 @@
 /**
-* id (integer)
-* username (text)
-* password (text)
-* phone (text?)
-* email (text?)
-* firstName (text?)
-* lastName (text?)
-* roleId (integer) [1: admin, 2: student, 3: teacher]
-* status (integer) [1: active, 2: inactive, -1: deleted]
-* credit (integer)
-* phoneVerifiedAt (timestamp?)
-* emailVerifiedAt (timestamp?)
-* createdAt (timestamp)
-* updatedAt (timestamp)
-*/
+ * id (integer)
+ * username (text)
+ * password (text)
+ * phone (text?)
+ * email (text?)
+ * firstName (text?)
+ * lastName (text?)
+ * roleId (integer) [1: admin, 2: student, 3: teacher]
+ * status (integer) [1: active, 2: inactive, -1: deleted]
+ * credit (integer)
+ * phoneVerifiedAt (timestamp?)
+ * emailVerifiedAt (timestamp?)
+ * createdAt (timestamp)
+ * updatedAt (timestamp)
+ */
 DROP TABLE IF EXISTS users;
-CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT NOT NULL, password TEXT NOT NULL, phone TEXT, email TEXT, firstName TEXT, lastName TEXT, status INTEGER NOT NULL, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL, roleId INTEGER NOT NULL, credit INTEGER NOT NULL, phoneVerifiedAt INTEGER, emailVerifiedAt INTEGER);
-INSERT INTO users (username, password, roleId, credit, createdAt, updatedAt, firstName, lastName, phone, phoneVerifiedAt, status) VALUES ('admin', 'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3', 1, 1000, 0, 0, 'Bon Jovi', 'Montes', '+639338520220', 1, 1), ('student', 'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3', 2, 1000, 0, 0, 'Joy', 'Liu', '+8618832258785', 1, 1), ('teacher', 'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3', 3, 0, 0, 0, 'Michelle', 'Surname', '+891234567890', 1, 1);
+
+CREATE TABLE
+  IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    phone TEXT,
+    email TEXT,
+    firstName TEXT,
+    lastName TEXT,
+    status INTEGER NOT NULL,
+    createdAt INTEGER NOT NULL,
+    updatedAt INTEGER NOT NULL,
+    roleId INTEGER NOT NULL,
+    credit INTEGER NOT NULL,
+    phoneVerifiedAt INTEGER,
+    emailVerifiedAt INTEGER
+  );
+
+INSERT INTO
+  users (
+    username,
+    password,
+    roleId,
+    credit,
+    createdAt,
+    updatedAt,
+    firstName,
+    lastName,
+    phone,
+    phoneVerifiedAt,
+    status
+  )
+VALUES
+  (
+    'admin',
+    'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3',
+    1,
+    1000,
+    0,
+    0,
+    'Bon Jovi',
+    'Montes',
+    '+639338520220',
+    1,
+    1
+  ),
+  (
+    'student',
+    'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3',
+    2,
+    1000,
+    0,
+    0,
+    'Joy',
+    'Liu',
+    '+8618832258785',
+    1,
+    1
+  ),
+  (
+    'teacher',
+    'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3',
+    3,
+    0,
+    0,
+    0,
+    'Michelle',
+    'Surname',
+    '+891234567890',
+    1,
+    1
+  );
 
 /**
-* id (integer)
-* userId (integer)
-* alias (text)
-* price (integer)
-* minutes (integer)
-* status (integer) [1: active, 2: inactive, -1: deleted]
-* createdAt (timestamp)
-* updatedAt (timestamp)
-*/
+ * id (integer)
+ * userId (integer)
+ * alias (text)
+ * price (integer)
+ * minutes (integer)
+ * status (integer) [1: active, 2: inactive, -1: deleted]
+ * createdAt (timestamp)
+ * updatedAt (timestamp)
+ */
 DROP TABLE IF EXISTS teachers;
-CREATE TABLE IF NOT EXISTS teachers (userId INTEGER PRIMARY KEY, id INTEGER UNIQUE NOT NULL, alias TEXT NOT NULL, price INTEGER NOT NULL, minutes INTEGER NOT NULL, status INTEGER NOT NULL, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL);
-INSERT INTO teachers (userId, id, price, alias, minutes, createdAt, updatedAt, status) VALUES (3, 202307311, 50, 'Teacher Michelle', 25, 0, 0, 1);
 
+CREATE TABLE
+  IF NOT EXISTS teachers (
+    userId INTEGER PRIMARY KEY,
+    id INTEGER UNIQUE NOT NULL,
+    alias TEXT NOT NULL,
+    price INTEGER NOT NULL,
+    minutes INTEGER NOT NULL,
+    status INTEGER NOT NULL,
+    createdAt INTEGER NOT NULL,
+    updatedAt INTEGER NOT NULL
+  );
+
+INSERT INTO
+  teachers (
+    userId,
+    id,
+    price,
+    alias,
+    minutes,
+    createdAt,
+    updatedAt,
+    status
+  )
+VALUES
+  (3, 202307311, 50, 'Teacher Michelle', 25, 0, 0, 1);
 
 /**
-* id (integer)
-* name (text)
-* createdAt (timestamp)
-* updatedAt (timestamp)
-*/
+ * id (integer)
+ * name (text)
+ * createdAt (timestamp)
+ * updatedAt (timestamp)
+ */
 DROP TABLE IF EXISTS roles;
-CREATE TABLE IF NOT EXISTS roles (id INTEGER PRIMARY KEY, name TEXT, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL);
-INSERT INTO roles (name, createdAt, updatedAt) VALUES ('admin', 0, 0), ('student', 0, 0), ('teacher', 0, 0);
+
+CREATE TABLE
+  IF NOT EXISTS roles (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    createdAt INTEGER NOT NULL,
+    updatedAt INTEGER NOT NULL
+  );
+
+INSERT INTO
+  roles (name, createdAt, updatedAt)
+VALUES
+  ('admin', 0, 0),
+  ('student', 0, 0),
+  ('teacher', 0, 0);
 
 /**
-* id (integer)
-* userId (integer)
-* startTime (timestampTimeOnly)
-* endTime (timestampTimeOnly)
-* day (integer)
-* createdAt (timestamp)
-* updatedAt (timestamp)
-*/
+ * id (integer)
+ * userId (integer)
+ * startTime (timestampTimeOnly)
+ * endTime (timestampTimeOnly)
+ * day (integer)
+ * createdAt (timestamp)
+ * updatedAt (timestamp)
+ */
 DROP TABLE IF EXISTS schedules;
-CREATE TABLE IF NOT EXISTS schedules (id INTEGER PRIMARY KEY, userId INTEGER NOT NULL, startTime INTEGER NOT NULL, endTime INTEGER NOT NULL, day INTEGER NOT NULL, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL);
+
+CREATE TABLE
+  IF NOT EXISTS schedules (
+    id INTEGER PRIMARY KEY,
+    userId INTEGER NOT NULL,
+    startTime INTEGER NOT NULL,
+    endTime INTEGER NOT NULL,
+    day INTEGER NOT NULL,
+    createdAt INTEGER NOT NULL,
+    updatedAt INTEGER NOT NULL
+  );
 
 /**
-* id (integer)
-* historyCredit (uuid)
-* teacherId (integer)
-* studentId (integer)
-* start (timestamp)
-* end (timestamp)
-* status (integer) [0:pending, 1: active, 2: finished, 3: cancelled, 4: absent, -1: deleted]
-* message (text)
-* createdAt (timestamp)
-* updatedAt (timestamp)
-*/
+ * id (integer)
+ * historyCredit (uuid)
+ * teacherId (integer)
+ * studentId (integer)
+ * start (timestamp)
+ * end (timestamp)
+ * status (integer) [0:pending, 1: active, 2: finished, 3: cancelled, 4: absent, -1: deleted]
+ * message (text)
+ * createdAt (timestamp)
+ * updatedAt (timestamp)
+ */
 DROP TABLE IF EXISTS bookings;
-CREATE TABLE IF NOT EXISTS bookings (id INTEGER PRIMARY KEY, historyCreditUuid TEXT UNIQUE NOT NULL, teacherId INTEGER NOT NULL, studentId INTEGER, start INTEGER NOT NULL, end INTEGER NOT NULL, status INTEGER NOT NULL, message TEXT, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL);
+
+CREATE TABLE
+  IF NOT EXISTS bookings (
+    id INTEGER PRIMARY KEY,
+    historyCreditUuid TEXT UNIQUE NOT NULL,
+    teacherId INTEGER NOT NULL,
+    studentId INTEGER,
+    start INTEGER NOT NULL,
+    end INTEGER NOT NULL,
+    status INTEGER NOT NULL,
+    message TEXT,
+    createdAt INTEGER NOT NULL,
+    updatedAt INTEGER NOT NULL
+  );
 
 /**
-* id (integer)
-* senderId (integer)
-* receiverId (integer)
-* amount (integer)
-* status (integer) [1: pending, 2: finished, 3: cancelled, -1: deleted]
-* createdAt (timestamp)
-* updatedAt (timestamp)
-*/
+ * id (integer)
+ * senderId (integer)
+ * receiverId (integer)
+ * amount (integer)
+ * status (integer) [1: pending, 2: finished, 3: cancelled, -1: deleted]
+ * createdAt (timestamp)
+ * updatedAt (timestamp)
+ */
 DROP TABLE IF EXISTS logsCredit;
-CREATE TABLE IF NOT EXISTS logsCredit (id INTEGER PRIMARY KEY, uuid TEXT UNIQUE NOT NULL, title TEXT, senderId INTEGER NOT NULL, receiverId INTEGER NOT NULL, amount INTEGER NOT NULL, status INTEGER NOT NULL, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL);
+
+CREATE TABLE
+  IF NOT EXISTS logsCredit (
+    id INTEGER PRIMARY KEY,
+    uuid TEXT UNIQUE NOT NULL,
+    title TEXT,
+    senderId INTEGER NOT NULL,
+    receiverId INTEGER NOT NULL,
+    amount INTEGER NOT NULL,
+    status INTEGER NOT NULL,
+    createdAt INTEGER NOT NULL,
+    updatedAt INTEGER NOT NULL
+  );
 
 /**
-* id (integer)
-* title (text)
-* senderId (integer)
-* receiverId (integer)
-* message (text)
-* cron (text)
-* sendAt (timestamp?)
-* status (integer) [1: active, 2: finished, 3: failed, 4: cancelled, -1: deleted]
-* createdAt (timestamp)
-* updatedAt (timestamp)
-*/
+ * id (integer)
+ * title (text)
+ * senderId (integer)
+ * receiverId (integer)
+ * message (text)
+ * cron (text)
+ * sendAt (timestamp?)
+ * status (integer) [1: active, 2: finished, 3: failed, 4: cancelled, -1: deleted]
+ * createdAt (timestamp)
+ * updatedAt (timestamp)
+ */
 DROP TABLE IF EXISTS messages;
-CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY, title TEXT NOT NULL, senderId INTEGER NOT NULL, receiverId INTEGER NOT NULL, message TEXT NOT NULL, cron TEXT NOT NULL, sendAt INTEGER, status INTEGER NOT NULL, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL);
-INSERT INTO messages (title, senderId, receiverId, message, cron, sendAt, status, createdAt, updatedAt) VALUES ('Phone Verification', 1, 2, '您的手机验证码是#code#，有效期仅5分钟。', '10 * * * *', 1691735400000, 1, 0, 0);
+
+CREATE TABLE
+  IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    senderId INTEGER NOT NULL,
+    receiverId INTEGER NOT NULL,
+    message TEXT NOT NULL,
+    cron TEXT NOT NULL,
+    sendAt INTEGER,
+    status INTEGER NOT NULL,
+    createdAt INTEGER NOT NULL,
+    updatedAt INTEGER NOT NULL
+  );
+
+INSERT INTO
+  messages (
+    title,
+    senderId,
+    receiverId,
+    message,
+    cron,
+    sendAt,
+    status,
+    createdAt,
+    updatedAt
+  )
+VALUES
+  (
+    'Phone Verification',
+    1,
+    2,
+    '您的手机验证码是#code#，有效期仅5分钟。',
+    '10 * * * *',
+    1691735400000,
+    1,
+    0,
+    0
+  );
 
 /**
-* id (integer)
-* smsId (integer) [From SMS api]
-* signature (text)
-* title (text)
-* message (text)
-* status (integer) [1: pending, 2: accepted, 3: rejected, -1: deleted]
-* createdAt (timestamp)
-* updatedAt (timestamp)
-*/
+ * id (integer)
+ * smsId (integer) [From SMS api]
+ * signature (text)
+ * title (text)
+ * message (text)
+ * status (integer) [1: pending, 2: accepted, 3: rejected, -1: deleted]
+ * createdAt (timestamp)
+ * updatedAt (timestamp)
+ */
 DROP TABLE IF EXISTS messageTemplates;
-CREATE TABLE IF NOT EXISTS messageTemplates (id INTEGER PRIMARY KEY, smsId INTEGER UNIQUE NOT NULL, signature TEXT NOT NULL, title TEXT NOT NULL, message TEXT NOT NULL, status INTEGER NOT NULL, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL);
-INSERT INTO messageTemplates (title, message, status, signature, smsId, createdAt, updatedAt) VALUES ('Phone Verification', '您的手机验证码是#code#，有效期仅5分钟。', 2, '恰恰英语', 5332, 0, 0), ('Class Reminder', '你好！您的课程安排在#date#，时间是#time#。我们将专注于口语练习。请准时到达，以充分利用本次课程。到时见！', 2, '恰恰英语', 5359, 0, 0);
+
+CREATE TABLE
+  IF NOT EXISTS messageTemplates (
+    id INTEGER PRIMARY KEY,
+    smsId INTEGER UNIQUE NOT NULL,
+    signature TEXT NOT NULL,
+    title TEXT NOT NULL,
+    message TEXT NOT NULL,
+    status INTEGER NOT NULL,
+    createdAt INTEGER NOT NULL,
+    updatedAt INTEGER NOT NULL
+  );
+
+INSERT INTO
+  messageTemplates (
+    title,
+    message,
+    status,
+    signature,
+    smsId,
+    createdAt,
+    updatedAt
+  )
+VALUES
+  (
+    'Phone Verification',
+    '您的手机验证码是#code#，有效期仅5分钟。',
+    2,
+    '恰恰英语',
+    5332,
+    0,
+    0
+  ),
+  (
+    'Class Reminder',
+    '你好！您的课程安排在#date#，时间是#time#。我们将专注于口语练习。请准时到达，以充分利用本次课程。到时见！',
+    2,
+    '恰恰英语',
+    5359,
+    0,
+    0
+  );

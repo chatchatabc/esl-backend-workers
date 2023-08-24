6cb28613a362ca404c1a7251e7fcf2c3dac0a19d
BoJoNVi <bonjomontes@gmail.com>
2023-08-24 16:03:41 +0800

    refactor: removed unnecessary endpoints in logs

--- src/application/trpc/logsRouter.ts
+++ src/application/trpc/logsRouter.ts
@@ -1,16 +1,10 @@
-import { merge, number, object } from "valibot";
 import {
   trpcProcedureAdmin,
   trpcProcedureUser,
   trpcRouterCreate,
 } from "../../domain/infra/trpc";
 import { CommonPaginationInput } from "../../domain/schemas/CommonSchema";
-import {
-  logsApproveCredit,
-  logsGetAllCredit,
-  logsRejectCredit,
-  logsRequestCredit,
-} from "../../domain/services/logsService";
+import { logsGetAllCredit } from "../../domain/services/logsService";
 import { utilFailedResponse } from "../../domain/services/utilService";
 
 export default trpcRouterCreate({
@@ -38,48 +32,5 @@ export default trpcRouterCreate({
       }
 
       return logsGetAllCredit({ ...opts.input, userId }, opts.ctx.env);
-    }),
-
-  approveCredit: trpcProcedureAdmin
-    .input((values: any = {}) => {
-      if (!values.logId) {
-        throw utilFailedResponse("Missing fields", 400);
-      }
-      const data = {
-        logId: values.logId,
-      } as { logId: number };
-      return data;
-    })
-    .mutation((opts) => {
-      return logsApproveCredit(opts.input, opts.ctx.env);
-    }),
-
-  rejectCredit: trpcProcedureAdmin
-    .input((values: any = {}) => {
-      if (!values.logId) {
-        throw utilFailedResponse("Missing fields", 400);
-      }
-      const data = {
-        logId: values.logId,
-      } as { logId: number };
-      return data;
-    })
-    .mutation((opts) => {
-      return logsRejectCredit(opts.input, opts.ctx.env);
-    }),
-
-  requestCredit: trpcProcedureAdmin
-    .input((values: any = {}) => {
-      if (!values.amount) {
-        throw utilFailedResponse("Missing fields", 400);
-      }
-      const data = {
-        amount: values.amount,
-      } as { amount: number };
-      return data;
-    })
-    .mutation((opts) => {
-      const { userId, env } = opts.ctx;
-      return logsRequestCredit({ ...opts.input, userId: userId ?? 0 }, env);
     }),
 });

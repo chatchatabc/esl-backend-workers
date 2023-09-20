f71e9a609050fb9f5c582cceac2855a207ee1b6a
BoJoNVi <bonjomontes@gmail.com>
2023-09-20 16:50:16 +0800

    feat: router endpoint for student create

--- src/application/trpc/studentRouter.ts
+++ src/application/trpc/studentRouter.ts
@@ -1,9 +1,11 @@
 import { trpcProcedureAdmin, trpcRouterCreate } from "../../domain/infra/trpc";
 import {
+  StudentCreateInput,
   StudentGetByUserInput,
   StudentGetInput,
 } from "../../domain/schemas/StudentSchema";
 import {
+  studentCreate,
   studentGet,
   studentGetByUser,
 } from "../../domain/services/studentService";
@@ -12,7 +14,14 @@ export default trpcRouterCreate({
   get: trpcProcedureAdmin.input(StudentGetInput).query((opts) => {
     return studentGet(opts.input, opts.ctx.env);
   }),
+
   getByUser: trpcProcedureAdmin.input(StudentGetByUserInput).query((opts) => {
     return studentGetByUser(opts.input, opts.ctx.env);
+  }),
+
+  create: trpcProcedureAdmin.input(StudentCreateInput).mutation((opts) => {
+    const { userId, env } = opts.ctx;
+
+    return studentCreate(opts.input, env, userId);
   }),
 });

dd51cd648ea14d22d706276b62c794a99146e8aa
BoJoNVi <bonjomontes@gmail.com>
2023-09-20 15:14:47 +0800

    feat: service for getting student by user

--- src/domain/services/studentService.ts
+++ src/domain/services/studentService.ts
@@ -1,6 +1,6 @@
 import { Env } from "../..";
 import { Student } from "../models/StudentModel";
-import { studentDbGet } from "../repositories/studentRepo";
+import { studentDbGet, studentDbGetByUser } from "../repositories/studentRepo";
 import { utilFailedResponse } from "./utilService";
 
 export async function studentGet(
@@ -12,6 +12,29 @@ export async function studentGet(
     throw utilFailedResponse("Student ID or UUID is required", 400);
   }
   const studentStmt = await studentDbGet(params, env);
+
+  try {
+    const query = await studentStmt.run<Student>();
+    const students = query.results;
+    if (!students.length) {
+      throw utilFailedResponse("Student not found", 404);
+    }
+    return students[0];
+  } catch (e) {
+    console.log(e);
+    throw utilFailedResponse("Cannot get student", 500);
+  }
+}
+
+export async function studentGetByUser(
+  params: Partial<{ userId: number; username: string }>,
+  env: Env
+) {
+  const { userId, username } = params;
+  if (!userId && !username) {
+    throw utilFailedResponse("User ID or username is required", 400);
+  }
+  const studentStmt = await studentDbGetByUser(params, env);
 
   try {
     const query = await studentStmt.run<Student>();

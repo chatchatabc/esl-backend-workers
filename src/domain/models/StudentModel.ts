97f4a440314fa0247980ad89b12c37c1b5572b65
BoJoNVi <bonjomontes@gmail.com>
2023-09-20 14:59:05 +0800

    feat: model for student

---
+++ src/domain/models/StudentModel.ts
@@ -0,0 +1,11 @@
+export type Student = {
+  id: number;
+  uuid: string;
+  userId: number;
+  alias: string | null;
+  bio: string | null;
+  status: number;
+  createdBy: number;
+  createdAt: string;
+  updatedAt: string;
+};

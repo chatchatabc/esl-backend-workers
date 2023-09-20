643cae59ba5c5c78d7b32f4ebd237380fa8df8e1
BoJoNVi <bonjomontes@gmail.com>
2023-09-20 15:13:26 +0800

    feat: repo for getting student by user

--- src/domain/repositories/studentRepo.ts
+++ src/domain/repositories/studentRepo.ts
@@ -12,13 +12,13 @@ export async function studentDbGet(
   let queryWhere = "";
 
   if (studentId) {
-    queryWhere += `id = ${studentId}`;
+    queryWhere += `id = ?`;
     queryParams.push(studentId);
   }
 
   if (uuid) {
     queryWhere += queryWhere ? " AND " : "";
-    queryWhere += `uuid = ${uuid}`;
+    queryWhere += `uuid = ?`;
     queryParams.push(uuid);
   }
 
@@ -32,6 +32,43 @@ export async function studentDbGet(
     const stmt = env.DB.prepare(query).bind(...queryParams);
     return stmt;
   } catch (e) {
+    console.log(e);
+    throw utilFailedResponse("Cannot generate student statement", 500);
+  }
+}
+
+export async function studentDbGetByUser(
+  params: Partial<{ userId: number; username: string }>,
+  env: Env
+) {
+  const { userId, username } = params;
+
+  const queryParams = [];
+  let query = "SELECT * FROM students";
+  let queryWhere = "";
+
+  if (userId) {
+    queryWhere += `userId = ?`;
+    queryParams.push(userId);
+  }
+
+  if (username) {
+    queryWhere += queryWhere ? " AND " : "";
+    queryWhere += `userId = (SELECT id FROM users WHERE username = ?)`;
+    queryParams.push(username);
+  }
+
+  if (queryWhere) {
+    query += ` WHERE ${queryWhere}`;
+  }
+
+  query += " LIMIT 1";
+
+  try {
+    const stmt = env.DB.prepare(query).bind(...queryParams);
+    return stmt;
+  } catch (e) {
+    console.log(e);
     throw utilFailedResponse("Cannot generate student statement", 500);
   }
 }

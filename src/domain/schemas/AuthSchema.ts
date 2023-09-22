78977d111d06345d58ac8c6198edce5469f8d3dc
BoJoNVi <bonjomontes@gmail.com>
2023-09-22 14:03:34 +0800

    feat: schema for auth login

---
+++ src/domain/schemas/AuthSchema.ts
@@ -0,0 +1,14 @@
+import { minLength, object, pick, string } from "valibot";
+
+export const AuthSchema = object({
+  username: string("Username must be a string", [
+    minLength(1, "Username is required"),
+  ]),
+  password: string("Password must be a string", [
+    minLength(1, "Password is required"),
+  ]),
+});
+
+export const AuthLoginInput = pick(AuthSchema, ["username", "password"]);
+
+

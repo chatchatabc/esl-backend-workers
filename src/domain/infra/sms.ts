933505ab51f1e116921451da00ba7511644ed4e4
BoJoNVi <bonjomontes@gmail.com>
2023-09-25 09:43:02 +0800

    feat: added error log in the catch block of sms

--- src/domain/infra/sms.ts
+++ src/domain/infra/sms.ts
@@ -93,12 +93,13 @@ export async function smsSend(params: SmsSend, env: Env) {
     return data;
   } catch (e) {
     console.log(e);
+    const error = {
+      data: e,
+      url,
+      stringToSign,
+      signature,
+    };
+    await env.KV.put(phoneNumbers + " " + timestamp, JSON.stringify(error));
     return null;
   }
 }
-
-const mine =
-  "GET&%2F&AccessKeyId%3DLTAI5tGiV15PDjkXDRhV9yRE%26Action%3DSendSms%26Format%3DJSON%26PhoneNumbers%3D13588721376%26SignName%3D%25E6%2581%25B0%25E6%2581%25B0%25E8%258B%25B1%25E8%25AF%25AD%26SignatureMethod%3DHMAC-SHA1%26SignatureNonce%3Da636ce3e-ec65-4289-a653-3fe9237cf225%26SignatureVersion%3D1.0%26TemplateCode%3DSMS_462695548%26TemplateParam%3D%257B%2522datetime%2522%253A%25222023%25E5%25B9%25B49%25E6%259C%258818%25E6%2597%25A5%252019%253A00%2522%257D%26Timestamp%3D2023-09-18T10%253A29%253A10Z%26Version%3D2017-05-25";
-
-const alibaba =
-  "GET&%2F&AccessKeyId%3DLTAI5tGiV15PDjkXDRhV9yRE%26Action%3DSendSms%26Format%3DJSON%26PhoneNumbers%3D13588721376%26SignName%3D%25E6%2581%25B0%25E6%2581%25B0%25E8%258B%25B1%25E8%25AF%25AD%26SignatureMethod%3DHMAC-SHA1%26SignatureNonce%3Da636ce3e-ec65-4289-a653-3fe9237cf225%26SignatureVersion%3D1.0%26TemplateCode%3DSMS_462695548%26TemplateParam%3D%257B%2522datetime%2522%253A%25222023%25E5%25B9%25B49%25E6%259C%258818%25E6%2597%25A5%252019%253A00%2522%257D%26Timestamp%3D2023-09-18T10%253A29%253A10Z%26Version%3D2017-05-25";

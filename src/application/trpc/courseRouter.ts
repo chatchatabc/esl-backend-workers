9d6b272bffe47a6d89b34817876e03652e82c554
BoJoNVi <bonjomontes@gmail.com>
2023-09-25 10:19:54 +0800

    refactor: credit controller input validation

--- src/application/trpc/courseRouter.ts
+++ src/application/trpc/courseRouter.ts
@@ -1,4 +1,3 @@
-import { parse } from "valibot";
 import {
   trpcProcedureAdmin,
   trpcProcedureUser,
--- src/application/trpc/creditRouter.ts
+++ src/application/trpc/creditRouter.ts
@@ -1,13 +1,10 @@
-import { parse } from "valibot";
 import { trpcProcedureAdmin, trpcRouterCreate } from "../../domain/infra/trpc";
 import { CreditAddInput } from "../../domain/schemas/CreditSchema";
 import { creditAdd } from "../../domain/services/creditService";
 
 export default trpcRouterCreate({
-  add: trpcProcedureAdmin
-    .input((input) => parse(CreditAddInput, input))
-    .mutation((opts) => {
-      const currency = "CNY";
-      return creditAdd({ ...opts.input, currency }, opts.ctx.env);
-    }),
+  add: trpcProcedureAdmin.input(CreditAddInput).mutation((opts) => {
+    const currency = "CNY";
+    return creditAdd({ ...opts.input, currency }, opts.ctx.env);
+  }),
 });

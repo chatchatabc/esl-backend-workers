dc43b7a2ba67c47ba8ed2554e00ef8e2f1f0718b
BoJoNVi <bonjomontes@gmail.com>
2023-09-25 10:22:24 +0800

    refactor: message template controller input validation

--- src/application/trpc/messageTemplateRouter.ts
+++ src/application/trpc/messageTemplateRouter.ts
@@ -1,6 +1,5 @@
-import { number, object, parse } from "valibot";
+import { number, object } from "valibot";
 import { trpcProcedureAdmin, trpcRouterCreate } from "../../domain/infra/trpc";
-import { CommonPaginationInput } from "../../domain/models/CommonModel";
 import {
   messageTemplateCreate,
   messageTemplateGet,
@@ -11,42 +10,33 @@ import {
   MessageTemplateCreateInput,
   MessageTemplateUpdateInput,
 } from "../../domain/schemas/MessageTemplateSchema";
+import { CommonPaginationInput } from "../../domain/schemas/CommonSchema";
 
 export default trpcRouterCreate({
   create: trpcProcedureAdmin
-    .input((input) => parse(MessageTemplateCreateInput, input))
+    .input(MessageTemplateCreateInput)
     .mutation((opts) => {
       return messageTemplateCreate(opts.input, opts.ctx.env);
     }),
 
   update: trpcProcedureAdmin
-    .input((input) => parse(MessageTemplateUpdateInput, input))
+    .input(MessageTemplateUpdateInput)
     .mutation((opts) => {
       return messageTemplateUpdate(opts.input, opts.ctx.env);
     }),
 
   get: trpcProcedureAdmin
-    .input((input) =>
-      parse(
-        object({
-          messageTemplateId: number("Message Template ID must be a number"),
-        }),
-        input
-      )
+    .input(
+      object({
+        messageTemplateId: number("Message Template ID must be a number"),
+      })
     )
     .query((opts) => {
       return messageTemplateGet(opts.input, opts.ctx.env);
     }),
 
-  getAll: trpcProcedureAdmin
-    .input((values: any = {}) => {
-      return {
-        page: values.page,
-        size: values.size,
-      } as CommonPaginationInput;
-    })
-    .query((opts) => {
-      const { page = 1, size = 10 } = opts.input;
-      return messageTemplateGetAll({ page, size }, opts.ctx.env);
-    }),
+  getAll: trpcProcedureAdmin.input(CommonPaginationInput).query((opts) => {
+    const { page = 1, size = 10 } = opts.input;
+    return messageTemplateGetAll({ page, size }, opts.ctx.env);
+  }),
 });

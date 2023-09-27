a5ca29efe271dfcfda70645aea599fe5ef5f9349
BoJoNVi <bonjomontes@gmail.com>
2023-09-27 10:29:31 +0800

    refactor: schema of credit entity

--- src/domain/schemas/CreditSchema.ts
+++ src/domain/schemas/CreditSchema.ts
@@ -1,9 +1,36 @@
-import { number, object, pick } from "valibot";
+import {
+  merge,
+  minLength,
+  minValue,
+  number,
+  object,
+  partial,
+  pick,
+  string,
+  transform,
+} from "valibot";
 
-const Schema = object({
-  userId: number("User ID is required"),
-  amount: number("Amount is required"),
-  credits: number("Credits is required"),
+export const CreditSchema = object({
+  userId: number("User ID must be a number", [
+    minValue(1, "User ID must be greater than 0"),
+  ]),
+  amount: number("Amount must be a number", [
+    minValue(1, "Amount must be greater than 0"),
+  ]),
+  credits: number("Credits must be a number", [
+    minValue(1, "Credits must be greater than 0"),
+  ]),
+  currency: string("Currency must be a number", [
+    minLength(3, "Currency must be greater than 2"),
+  ]),
 });
 
-export const CreditAddInput = pick(Schema, ["userId", "amount", "credits"]);
+export const CreditAddInput = transform(
+  merge([
+    pick(CreditSchema, ["userId", "amount", "credits"]),
+    partial(pick(CreditSchema, ["currency"])),
+  ]),
+  (input) => {
+    return { ...input, currency: input.currency ?? "CNY" };
+  }
+);
